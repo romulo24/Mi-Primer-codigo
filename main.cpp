8bdfa6a85d38a74d488789067c26c2685714888f
Romulo Torres <torresromulo200@gmail.com>
2021-07-03 19:12:43 -0500

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Es te es un ejemplo para GitHub!" << endl;
+    for(int i=0;i<5;i++){
+        cout<<"Que genial es programar"<<endl;
+    }
+    return 0;
+}
